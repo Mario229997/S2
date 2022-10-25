413be0099b324971c7022dd8673eb5f885eb80f2
Mario229997 <mario.torres01@estudiant.upf.edu>
2022-10-25 23:24:01 +0200

    update

--- S2_Ex2.py
+++ S2_Ex2.py
@@ -1,13 +1,23 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def process_video(video_filepath):
     cap = cv2.VideoCapture(video_filepath)
     grabbed, frame = cap.read()
 
+    count = 0
     while grabbed:
-        grabbed, frame = cap.read()
-        # do stuff with `frame` here
+        grabbed, frame = cap.read()                             # read every frame of the video
+        frame_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)      # convert the frame from rgb to yuv
+        frame_hist = np.histogram(frame_yuv)                    # compute histogram of the yuv frame
+        fig = plt.plot(frame_hist)                              # plot histogram
+        cv2.imwrite("Histograms_Ex2/frame%d.jpg" % count, fig)  # save each plot into Histograms_Ex2 folder
+        plt.close(fig)
+        count += 1
     cap.release()
 
-    
+
+input_video = 'BBB_11.mp4'
+process_video(input_video)
--- S2_Ex3.py
+++ S2_Ex3.py
@@ -1,17 +1,21 @@
 import moviepy.editor as mp
 
+# Video resized to 720p
 clip = mp.VideoFileClip("BBB_11.mp4")
 clip_resized = clip.resize(height=720)
 clip_resized.write_videofile("Videos_Ex3/BBB_720.mp4")
 
+# Video resized to 480p
 clip = mp.VideoFileClip("BBB_11.mp4")
 clip_resized = clip.resize(height=480)
 clip_resized.write_videofile("Videos_Ex3/BBB_480.mp4")
 
+# Video resized to 360x240
 clip = mp.VideoFileClip("BBB_11.mp4")
 clip_resized = clip.resize(newsize=(360, 240))
 clip_resized.write_videofile("Videos_Ex3/BBB_360x240.mp4")
 
+# Video resized to 160x120
 clip = mp.VideoFileClip("BBB_11.mp4")
 clip_resized = clip.resize(newsize=(160, 120))
 clip_resized.write_videofile("Videos_Ex3/BBB_160x120.mp4")
